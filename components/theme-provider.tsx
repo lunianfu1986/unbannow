9b53892134edc2c86fc6569d06a572ed702a428c
lunianfu1986 <lunianfu1986@gmail.com>
2025-11-30 00:09:50 +0800

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,16 +2,22 @@
 
 import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { type ThemeProviderProps } from 'next-themes/dist/types'
+import type { ThemeProviderProps } from 'next-themes'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({
+  children,
+  ...props
+}: ThemeProviderProps & { children: React.ReactNode }) {
   return (
-    <NextThemesProvider 
+    <NextThemesProvider
       {...props}
-      suppressHydrationWarning
       attribute="class"
       defaultTheme="system"
       enableSystem
+      // 如果你有配置过自定义主题列表，也可以在这里加：
+      // themes={['light', 'dark']}
+      // disableTransitionOnChange 也可以按需加：
+      // disableTransitionOnChange
     >
       {children}
     </NextThemesProvider>
