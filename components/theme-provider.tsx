6fa48c57772f1299ba13ea924a2d4ff3b6123b06
lunianfu1986 <lunianfu1986@gmail.com>
2025-11-30 00:13:49 +0800

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,22 +2,21 @@
 
 import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import type { ThemeProviderProps } from 'next-themes'
 
-export function ThemeProvider({
-  children,
-  ...props
-}: ThemeProviderProps & { children: React.ReactNode }) {
+// 关键：不再从 'next-themes' 里 import 类型，
+// 而是用 React.ComponentProps<typeof NextThemesProvider> 来推导类型。
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider
       {...props}
       attribute="class"
       defaultTheme="system"
       enableSystem
-      // 如果你有配置过自定义主题列表，也可以在这里加：
-      // themes={['light', 'dark']}
-      // disableTransitionOnChange 也可以按需加：
+      // 按需可以开启/关闭这些：
       // disableTransitionOnChange
+      // themes={['light', 'dark']}
     >
       {children}
     </NextThemesProvider>
