ee967779fc1cb6f94f92827ca847bda80c3a65bf
lunianfu1986 <lunianfu1986@gmail.com>
2025-12-01 14:57:58 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // 允许的图片域名（你现在用的是 Unsplash）
-    domains: ['images.unsplash.com'],
-    // 关闭 next/image 的内置优化，直接原图输出
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
 
   // 301 重定向：老地址 /blog/:slug -> 新地址 /:slug
