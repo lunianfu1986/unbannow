5594d78ca2c25729d481793069cb74549a4de1e3
lunianfu1986 <lunianfu1986@gmail.com>
2025-12-01 23:12:46 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,18 +14,12 @@ const nextConfig = {
         source: '/blog/:slug',
         destination: '/:slug',
         permanent: true,
-      },
-    ]
-  },
-}
-async redirects() {
-    return [
-      {
         source: '/admin',
         destination: '/admin/index.html',
         permanent: true, // 开启永久重定向 (308)
       },
     ]
   },
+}
 
 module.exports = nextConfig
