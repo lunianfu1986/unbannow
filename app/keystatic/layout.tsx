93d9aa1d440859273eb95e4775db911780b7a7c2
lunianfu1986 <lunianfu1986@gmail.com>
2026-02-03 23:45:22 +0800

    Create layout.tsx

---
+++ app/keystatic/layout.tsx
@@ -0,0 +1,13 @@
+export default function KeystaticLayout({
+     children,
+   }: {
+     children: React.ReactNode
+   }) {
+     return (
+       <html lang="en">
+         <body>
+           {children}
+         </body>
+       </html>
+     )
+   }
