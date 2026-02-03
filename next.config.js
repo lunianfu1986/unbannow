08a9d1eb88a155cb2ddb48e3311083e1357ded91
lunianfu1986 <lunianfu1986@gmail.com>
2026-02-03 08:51:15 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,29 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    // 允许的图片域名
-    domains: ['images.unsplash.com'],
-    // 关闭 next/image 的内置优化
-    unoptimized: true,
+  experimental: {
+    serverActions: true,
   },
+};
 
-  // 301/308 重定向配置
-  async redirects() {
-    return [
-      // 规则 1：博客文章重定向
-      {
-        source: '/blog/:slug',
-        destination: '/:slug',
-        permanent: true,
-      },
-      // 规则 2：后台管理重定向 (注意这里是一个新的对象，用逗号分隔)
-      {
-        source: '/admin',
-        destination: '/admin/index.html',
-        permanent: true,
-      },
-    ]
-  },
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
